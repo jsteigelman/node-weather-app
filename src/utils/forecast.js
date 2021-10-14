2b7680fad8c1253f7ae926f8639cc50b2ed541ed
Joey Steigelman <joey.steigelman@gmail.com>
2021-10-14 17:36:57 +0200

    added second access key since i exceeded monthly limit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (coord1, coord2, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=a20fe3be251c6fb71f004e8a631c4182&query=' + coord1 + ',' + coord2 + '&units=f'
+    // const url = 'http://api.weatherstack.com/current?access_key=a20fe3be251c6fb71f004e8a631c4182&query=' + coord1 + ',' + coord2 + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=e8cf670cd359e60f1532d9c16c382432&query=' + coord1 + ',' + coord2 + '&units=f'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
